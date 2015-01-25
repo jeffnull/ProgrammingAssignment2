b6d8ef424948a2bf38f38edf696c604f252af4bb
Jeff Noel <noel.set@gmail.com>
2015-01-25 16:30:11 -0500

    added functionality to makeCacheMatrix and cacheSolve stubs

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,53 @@
 ## Created: Jan 25, 2015
 ## Edited: Jan 25, 2015
 
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Create a vector containing functions to get and set the entries of a matrix,
+##   as well as functions to get and set the defined value of its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-    ## test edit
-
+    
+    ## matrix's inverse not defined until cacheSolve is called
+    xInverse <- NULL
+    
+    ## Sets the matrix's entries to the argument. Inverse will become undefined.
+    set <- function(y) {
+        x <<- y
+        xInverse <<- NULL
+    }
+    
+    ## return the entries of the matrix
+    get <- function() x
+    
+    ## Used by cacheSolve to store the marix's inverse
+    setInverse <- function(inverse) xInverse <<- inverse
+    
+    ## return the stored value of the matrix inverse (or NULL)
+    getInverse <- function() xInverse
+    
+    ## packages together the defined functions into an output
+    list (set = set, get = get, setInverse = setInverse,
+          getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## Takes the matrix structure defined in makeCacheMatrix and outputs the
+## matrix's inverse. Initially, it computes this via solve, but subsequently
+## uses the value computed in previous calls.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   
+    
+    ## If the inverse was already cached, return it.
+    xInverse <- x$getInverse()
+    if(!is.null(xInverse)) {
+        message("Using cached matrix inverse.")
+        return (xInverse)
+    }
+    
+    ## Otherwise, get the matrix entries and compute the inverse via solve().
+    ## Save the result of this computation within the matrix structure.
+    matrixEntries <- x$get()
+    inverse <- solve(matrixEntries, ...)
+    x$setInverse(inverse)
+    inverse
 }
